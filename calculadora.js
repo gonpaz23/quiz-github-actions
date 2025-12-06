32c1557469c0f8fb2a09307ff3fdf25d94b8d79e
Cristhian J. Paz <156976182+gonpaz23@users.noreply.github.com>
2025-12-06 23:59:44 +0000

    ejercicio2_c2_1

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+module.exports = { sumar, restar, multiplicar };
+
